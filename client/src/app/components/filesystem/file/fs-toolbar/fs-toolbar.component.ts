0b9eaa205e95309a5ba143a2854ba295cc645949
qfilip <kukolj.filip@gmail.com>
2025-03-13 00:11:42 +0100

    delete from toolbar implemented

--- client/src/app/components/filesystem/file/fs-toolbar/fs-toolbar.component.ts
+++ client/src/app/components/filesystem/file/fs-toolbar/fs-toolbar.component.ts
@@ -5,6 +5,10 @@ import { ROOT } from '../../fsConstants';
 import { FsItemCreateDialog } from '../fs-item-create-dialog/fs-item-create-dialog.dialog';
 import { FsItem } from '../fsitem.models';
 import { FsItemStateService } from '../fsItemState.service';
+import { FsItemUtils } from '../fsitem.utils';
+import { DialogService } from '../../../common-ui/simple-dialog/dialog.service';
+import { FsBranchStateService } from '../../branch/fsBranchState.service';
+import { PopupService } from '../../../common-ui/popup/popup.service';
 
 @Component({
   selector: 'app-fs-toolbar',
@@ -15,7 +19,10 @@ import { FsItemStateService } from '../fsItemState.service';
 export class FsToolbarComponent {
   @ViewChild('createDialog') private createDialog!: FsItemCreateDialog;
   
+  private popup = inject(PopupService);
+  private dialogService = inject(DialogService);
   private fsItemStateService = inject(FsItemStateService);
+  private fsBranchStateService = inject(FsBranchStateService);
   
   $selected = computed(() => this.fsItemStateService.$selected());
   
@@ -26,5 +33,30 @@ export class FsToolbarComponent {
   async openCreateDialog(fsi: FsItem) {
     const root = this.fsItemStateService.$root();
     this.createDialog.open(fsi, root!);
+  }
+
+  delete() {
+    const item = this.$selected()!;
+
+    if(item.id === ROOT.id) {
+      this.dialogService.openInfo('Root directory cannot be deleted');
+      return;
+    }
+    
+    const dds = FsItemUtils.getChildren(item);
+    const message = `
+      This action will delete selected item and all of its children:
+      directories: ${dds.dirs.length},
+      documents: ${dds.docs.length},
+      Do you wish to proceed?
+    `;
+
+    const execute = () => {
+      const event = FsItemUtils.createFsItemEvent('deleted', item as FsItem);
+      this.fsBranchStateService.addEvent(event);
+      this.popup.ok('File changed');
+    }
+
+    this.dialogService.openCheck(message, () => execute());
   }
 }
