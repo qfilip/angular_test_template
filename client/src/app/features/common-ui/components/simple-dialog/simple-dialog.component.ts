dea5b196c2a483ee84249192201b087e9d8c26d7
qfilip <filip.kukolj@arcadis.com>
2025-04-03 14:37:45 +0200

    dialog using native dialog

--- client/src/app/features/common-ui/components/simple-dialog/simple-dialog.component.ts
+++ client/src/app/features/common-ui/components/simple-dialog/simple-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, inject, signal } from '@angular/core';
+import { Component, effect, ElementRef, inject, signal, ViewChild } from '@angular/core';
 
 import { DialogOptions } from '../../models/dialog-options.model';
 import { DialogService } from '../../services/dialog.service';
@@ -10,13 +10,25 @@ import { DialogService } from '../../services/dialog.service';
   styleUrl: './simple-dialog.component.css'
 })
 export class SimpleDialogComponent {
+  @ViewChild('dialog') private dialog!: ElementRef<HTMLDialogElement>;
   private service = inject(DialogService);
   private _$options = signal<DialogOptions | null>(null);
 
   $options = this._$options.asReadonly();
 
   constructor() {
-    effect(() => this._$options.set(this.service.$dialogOptions()))
+    effect(() => {
+      const opts = this.service.$dialogOptions();
+      this._$options.set(opts);
+
+      if(!this.dialog) return;
+      
+      const action = opts !== null
+        ? () => this.dialog.nativeElement.showModal()
+        : () => this.dialog.nativeElement.close();
+
+      action();
+    });
   }
   
   onButtonClicked(btnAction: () => void) {
